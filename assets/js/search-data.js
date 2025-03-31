3939c32777d794efe7cf669842fcf5e8f619884c
wchongmh <wchongmh@gmail.com>
2025-03-31 14:03:05 +0000

    Deploying to gh-pages from @ wchongmh/wchongmh.github.io@ac58909f2b322da321b00e59851aec27fed67d9a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,13 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-award-gallery",
-          title: "Award Gallery",
-          description: "Moments from my award journey",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/award-gallery/";
-          },
         },{id: "nav-cv",
           title: "CV",
           description: "Award-winning UK Engineering graduate with a strong technical foundation and leadership acumen, specializing in innovative building services solutions. Recognized with the SCEBE Prize for top academic performance and fully funded through the Alistair Harvey Foundation Scholarship to complete BEng/MSc degrees.",
