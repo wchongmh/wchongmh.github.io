62bc1ea4019c264901e7575a4f9171c53c5f12c9
wchongmh <wchongmh@gmail.com>
2025-03-31 11:57:08 +0000

    Deploying to gh-pages from @ wchongmh/wchongmh.github.io@3f0438e7c7d85e8368ef6ec4d32f281a292dc216 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -10,7 +10,7 @@ ninja.data = [{
       window.location.href = "/";
     },
   },{id: "nav-projects",
-          title: "projects",
+          title: "Projects",
           description: "A growing collection of your cool projects.",
           section: "Navigation",
           handler: () => {
