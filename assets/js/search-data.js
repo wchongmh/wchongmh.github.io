28d8b90b551175fa0db56e80cfcc706622bfa324
wchongmh <wchongmh@gmail.com>
2025-04-15 22:03:02 +0000

    Deploying to gh-pages from @ wchongmh/wchongmh.github.io@f33f4ca28167fd13eeb4aa4834ec8dec61739b0a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -488,6 +488,11 @@ ninja.data = [{
           description: "Comprehensive plumbing design for 3,120-unit HK housing project, including hydraulic calculations and system schematics.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/queens-hill-plumbing/";
+            },},{id: "projects-high-rise-plumbing-amp-fire-protection-systems-axa-center",
+          title: 'High-Rise Plumbing &amp;amp; Fire Protection Systems - AXA Center',
+          description: "Integrated plumbing and fire protection design for 30-storey commercial office tower in Wan Chai",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/test/";
             },},{
         id: 'social-email',
         title: 'email',
