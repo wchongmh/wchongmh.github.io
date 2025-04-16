457af34e92b80183914156f18ffc56f11318cc18
wchongmh <wchongmh@gmail.com>
2025-04-16 10:53:54 +0000

    Deploying to gh-pages from @ wchongmh/wchongmh.github.io@22c2dc7659fd04bc4a6dae8e0e743b3df2e27139 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -485,14 +485,14 @@ ninja.data = [{
               window.location.href = "/projects/queens-hill-plumbing/";
             },},{id: "projects-venturi-nozzle-flow-measurement-analysis",
           title: 'Venturi Nozzle Flow Measurement Analysis',
+          description: "Experimental analysis of water flow rate using Venturi nozzle principles at Glasgow Caledonian University",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/venturi-lab*/";
+            },},{id: "projects-venturi-nozzle-flow-measurement-analysis",
+          title: 'Venturi Nozzle Flow Measurement Analysis',
           description: "Experimental analysis of flow rate measurement using Venturi nozzle principles at Glasgow Caledonian University",
           section: "Projects",handler: () => {
               window.location.href = "/projects/venturi-lab/";
-            },},{id: "projects-venturi-nozzle-flow-measurement-analysis",
-          title: 'Venturi Nozzle Flow Measurement Analysis',
-          description: "Experimental analysis of water flow rate using Venturi nozzle principles at Glasgow Caledonian University",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/venturi-nozzle/";
             },},{
         id: 'social-email',
         title: 'email',
