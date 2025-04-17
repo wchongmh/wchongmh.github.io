10659c36096d081eb36a7345ee2a145cadfc763b
wchongmh <wchongmh@gmail.com>
2025-04-17 18:15:17 +0000

    Deploying to gh-pages from @ wchongmh/wchongmh.github.io@45ced2130bdc65426e7b972c50dca7e129511603 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -493,13 +493,13 @@ ninja.data = [{
           description: "Experimental analysis of pressure losses in pipes and fittings using HM 150.11 module at Glasgow Caledonian University.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/pressure02/";
-            },},{id: "projects-pressure-losses-in-piping-systems-laboratory-analysis04",
-          title: 'Pressure Losses in Piping Systems - Laboratory Analysis04',
+            },},{id: "projects-pressure-losses-in-piping-systems-laboratory-analysis",
+          title: 'Pressure Losses in Piping Systems - Laboratory Analysis',
           description: "Experimental measurement and analysis of pressure losses in pipes and fittings using HM 150.11 module at Glasgow Caledonian University.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/pressure03/";
-            },},{id: "projects-pressure-losses-in-piping-systems-laboratory-analysis05",
-          title: 'Pressure Losses in Piping Systems - Laboratory Analysis05',
+            },},{id: "projects-pressure-losses-in-piping-systems-laboratory-analysis",
+          title: 'Pressure Losses in Piping Systems - Laboratory Analysis',
           description: "Experimental analysis of pressure losses in pipes and fittings using HM 150.11 module at Glasgow Caledonian University",
           section: "Projects",handler: () => {
               window.location.href = "/projects/pressure_test/";
