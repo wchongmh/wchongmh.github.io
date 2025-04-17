a4bfc9dc814f2eda84b7df2fc8a06212e9449a2e
wchongmh <wchongmh@gmail.com>
2025-04-17 18:13:54 +0000

    Deploying to gh-pages from @ wchongmh/wchongmh.github.io@8beceb6c5adfe064dc9fae23fc1de233ac6844cd 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -483,8 +483,8 @@ ninja.data = [{
           description: "Experimental analysis of pressure losses in pipes and fittings using HM 150.11 module at Glasgow Caledonian University",
           section: "Projects",handler: () => {
               window.location.href = "/projects/pressure-drop/";
-            },},{id: "projects-pressure-loss-analysis-in-piping-systems",
-          title: 'Pressure Loss Analysis in Piping Systems',
+            },},{id: "projects-pressure-loss-analysis-in-piping-systems02",
+          title: 'Pressure Loss Analysis in Piping Systems02',
           description: "Experimental analysis of pressure losses in pipes and fittings using HM 150.11 module at Glasgow Caledonian University",
           section: "Projects",handler: () => {
               window.location.href = "/projects/pressure01/";
