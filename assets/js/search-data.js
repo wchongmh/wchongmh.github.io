eddad23c0a39d1326d2bbd6db0932d13d06467a3
wchongmh <wchongmh@gmail.com>
2025-04-17 15:08:43 +0000

    Deploying to gh-pages from @ wchongmh/wchongmh.github.io@da76c21f6ab221f0579317dda61beb47cbd0f0f1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -508,6 +508,11 @@ ninja.data = [{
           description: "Field study of non-invasive flow measurement techniques in HVAC systems using ultrasonic technology.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/ultrasonic02/";
+            },},{id: "projects-ultrasonic-flow-rate-measurements-gcu-plant-room",
+          title: 'Ultrasonic Flow Rate Measurements - GCU Plant Room',
+          description: "Field study of non-invasive flow measurement techniques in HVAC systems using ultrasonic technology.",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/ultrasonic03/";
             },},{id: "projects-venturi-nozzle-flow-measurement-analysis",
           title: 'Venturi Nozzle Flow Measurement Analysis',
           description: "Experimental analysis of flow rate measurement using Venturi nozzle principles at Glasgow Caledonian University",
