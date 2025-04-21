f9b1cff293565bb5198e500700aabd2c9d7c0551
wchongmh <wchongmh@gmail.com>
2025-04-21 07:07:32 +0000

    Deploying to gh-pages from @ wchongmh/wchongmh.github.io@2309f9eae600dcf4081d7c91ac44facd4f462b13 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -482,11 +482,6 @@ ninja.data = [{
           title: 'Daylight and Lighting Design - Glasgow Residential Project',
           description: "Comprehensive daylight analysis and lighting design for a 2-storey semi-detached house in Glasgow using Dialux simulations and manual calculations.",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/ies02/";
-            },},{id: "projects-daylight-and-lighting-design-glasgow-residential-project",
-          title: 'Daylight and Lighting Design - Glasgow Residential Project',
-          description: "Comprehensive daylight analysis and lighting design for a 2-storey semi-detached house in Glasgow using Dialux simulations and manual calculations.",
-          section: "Projects",handler: () => {
               window.location.href = "/projects/ies022/";
             },},{id: "projects-daylighting-design-analysis-glasgow-residential-project",
           title: 'Daylighting Design Analysis - Glasgow Residential Project',
