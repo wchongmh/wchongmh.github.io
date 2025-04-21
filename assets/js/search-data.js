ad86438c34dcafd1ac7be2ec4ace14104e6b4468
wchongmh <wchongmh@gmail.com>
2025-04-21 11:51:49 +0000

    Deploying to gh-pages from @ wchongmh/wchongmh.github.io@65a6eff9f3aca11a3bb25f46b9c91b7d11fba9fb 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -458,6 +458,11 @@ ninja.data = [{
           description: "Comprehensive daylight analysis for residential property in Glasgow using Dialux, including manual calculations and energy efficiency evaluation.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/dialux/";
+            },},{id: "projects-daylighting-design-analysis-glasgow-residential-project",
+          title: 'Daylighting Design Analysis - Glasgow Residential Project',
+          description: "Comprehensive daylighting analysis for a 2-storey semi-detached house in Glasgow using manual calculations and Dialux simulations.",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/dialux011/";
             },},{id: "projects-dialux-lighting-design-commercial-building-project",
           title: 'DIALux Lighting Design - Commercial Building Project',
           description: "Comprehensive lighting design for a mixed-use commercial building featuring offices, supermarket, and coffee shop using DIALux software.",
@@ -483,11 +488,6 @@ ninja.data = [{
           description: "Comprehensive energy analysis and modifications for a Glasgow office building to comply with Section 6 of Scottish Technical Standards using IES VE software.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/ies01/";
-            },},{id: "projects-daylighting-design-analysis-glasgow-residential-project",
-          title: 'Daylighting Design Analysis - Glasgow Residential Project',
-          description: "Comprehensive daylighting analysis for a 2-storey semi-detached house in Glasgow using manual calculations and Dialux simulations.",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/ies011/";
             },},{id: "projects-daylight-and-lighting-design-glasgow-residential-project",
           title: 'Daylight and Lighting Design - Glasgow Residential Project',
           description: "Comprehensive daylight analysis and lighting design for a 2-storey semi-detached house in Glasgow using Dialux simulations and manual calculations.",
