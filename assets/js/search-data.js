f297a239dd391764810aa8b673064f4e5b1224e0
wchongmh <wchongmh@gmail.com>
2025-04-24 21:04:16 +0000

    Deploying to gh-pages from @ wchongmh/wchongmh.github.io@6a0de84d7d2a0429e6dd77f18d965de33c8451f9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -473,6 +473,11 @@ ninja.data = [{
           description: "Comprehensive fire safety design for a Scottish residential care home, focusing on vulnerable occupants and regulatory compliance.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/care-home04/";
+            },},{id: "projects-fire-service-system-design-residential-care-home",
+          title: 'Fire Service System Design - Residential Care Home',
+          description: "Comprehensive fire safety design for a Scottish residential care home, focusing on vulnerable occupants and regulatory compliance.",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/care-home05/";
             },},{id: "projects-daylighting-design-analysis-glasgow-residential-project",
           title: 'Daylighting Design Analysis - Glasgow Residential Project',
           description: "Comprehensive daylight analysis for residential property in Glasgow using Dialux, including manual calculations and energy efficiency evaluation.",
