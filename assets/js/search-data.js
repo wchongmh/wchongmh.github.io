607f056eb6d4d6e92ed01cb0c31d76db12e9391e
wchongmh <wchongmh@gmail.com>
2025-04-25 20:50:26 +0000

    Deploying to gh-pages from @ wchongmh/wchongmh.github.io@596336201f6d69973f314c52d971ed74f6e413a9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -460,11 +460,6 @@ ninja.data = [{
               window.location.href = "/projects/care-home/";
             },},{id: "projects-fire-service-system-design-residential-care-home",
           title: 'Fire Service System Design - Residential Care Home',
-          description: "Comprehensive fire safety design for a Scottish residential care home, focusing on vulnerable occupants and regulatory compliance.",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/care-home02/";
-            },},{id: "projects-fire-service-system-design-residential-care-home",
-          title: 'Fire Service System Design - Residential Care Home',
           description: "Comprehensive fire safety system design for a Scottish residential care home, focusing on vulnerable occupants and regulatory compliance.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/care-home03/";
